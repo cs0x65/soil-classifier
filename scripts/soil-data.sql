da1310e1df762c4ee3292771eeccc69329d2df78
Saurabh Sirdeshmukh <saurabh.cse2@gmail.com>
2020-09-08 23:47:43 +0530

    - adds requirements to use with virtualenv: python 3.7.x is being used, will be using scikit-learn. - adds sql for soil profile table and importing dataset csv to it: to be converted into a shell script - to be a part of setup scripts.

---
+++ scripts/soil-data.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS soil_profile(
+id serial primary key,
+state varchar, district varchar, taluk varchar, village varchar, soil_type varchar, authority varchar,
+ph float, ec float, oc float,
+av_p float, av_K float,
+av_s float, av_zn float, av_b float, av_fe float, av_cu float, av_mn float
+);
+
+"COPY soil_profile(
+state, district, taluk, village, soil_type, authority,
+ph, ec, oc, av_p, av_k,	av_s, av_zn, av_b, av_fe, av_cu,av_mn
+)
+FROM '/Users/saurabh/github/soil-classifier/dataset/soil-profile-data.csv' DELIMITER ',' CSV HEADER;"
+
+cat /Users/saurabh/github/soil-classifier/dataset/soil-profile-data.csv | psql sa -d soildb -h 127.0.0.1 -c
+"COPY soil_profile(
+state, district, taluk, village, soil_type, authority, ph, ec, oc, av_p, av_k,  av_s, av_zn, av_b, av_fe, av_cu,av_mn)
+FROM stdin DELIMITER ',' CSV HEADER;"
+Password for user sa:
+COPY 92832
+
+
+
+
+
+
+
+
