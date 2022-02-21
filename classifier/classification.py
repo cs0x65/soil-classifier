8fadc2e45b2d292137f3a8b426188d7c76d4919e
Saurabh Sirdeshmukh <saurabh.cse2@gmail.com>
2022-02-22 02:03:59 +0530

    - refactors soil classifier such that the specific instances of classes like, multiclass & multilabel classifiers adopt the specific algorithms/models

--- classifier/classification.py
+++ classifier/classification.py
@@ -24,87 +24,34 @@ class SoilClassifier(object):
 
     def classify(self):
         self.data = pandas.read_csv(self.csv_file)
-        self.single_feature_multiple_classes(features=['ph', 'ec'])
-        self._single_feature_multiple_labels()
+        MulticlassClassifier(self.data).single_feature_multiclass_classify(features=['ph', 'ec'])
+        MultilabelClassifier(self.data).single_feature_multilabel_classify(features=['ph'])
 
-    def single_feature_multiple_classes(self, features: List):
+
+class MulticlassClassifier(object):
+    def __init__(self, data):
+        self.data = data
+
+    def single_feature_multiclass_classify(self, features: List):
         """
         This method acts on a single feature and applies classification algorithms such that each record in the
         test dataset on prediction belongs to one & only one class from the classes supplied in the training data.
         :param features: list of features for each of which the classification models are trained and predicted.
         :return:
         """
-        print(f'--Start of single feature binary labels classification for: {features}--')
+        print(f'--Start of single feature multi-class classification for: {features}--')
 
         for feature in features:
-            self._knn_classify(features=[feature], multilabel=False, plot=False)
+            self._knn_classify(features=[feature], multilabel=False, plot=True)
             self._svn_classify(features=[feature], multilabel=False)
 
-        print(f'--End of single feature binary labels classification for: {features}--')
-
-    def _single_feature_multiple_labels(self):
-        print('--Start of pH multilabel classification--')
-
-        x = self.data.drop([GroundTruthBuilder.PH_GENERIC_CLASS, GroundTruthBuilder.PH_VEG_ROW_CROPS_CLASS,
-                            GroundTruthBuilder.PH_FRUITS_NUTS_CLASS], axis=1)
-        x = x.iloc[:, 6:7]
-        y = self.data[['ph_veg_and_row_crops', 'ph_fruits_and_nuts']]
-
-        x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.30, random_state=31)
-
-        print('Training model: BinaryRelevance')
-        # initialize binary relevance multi-label classifier with a gaussian naive bayes base classifier
-        classifier = BinaryRelevance(GaussianNB())
-        classifier.fit(x_train, y_train)
-        print('Predictions: BinaryRelevance')
-        predictions = classifier.predict(x_test)
-        print(f'Multilabel BinaryRelevance accuracy = \n{accuracy_score(predictions, y_test)}')
-        print(f'Multilabel BinaryRelevance classification report = \n{classification_report(predictions, y_test)}')
-        # print(f'Multilabel confusion matrix = \n{confusion_matrix(predictions, y_test)}')
-
-        print('Training model: ClassifierChain')
-        classifier = ClassifierChain(GaussianNB())
-        diff = set(y_test) - set(y_train)
-        classifier.fit(x_train, y_train)
-        print('Predictions: ClassifierChain')
-        predictions = classifier.predict(x_test)
-        print(f'Multilabel ClassifierChain accuracy = \n{accuracy_score(predictions, y_test)}')
-        print(f'Multilabel ClassifierChain classification report = \n{classification_report(predictions, y_test)}')
-
-        print('Training model: LabelPowerset GNB')
-        classifier = LabelPowerset(GaussianNB())
-        classifier.fit(x_train, y_train)
-        print('Predictions: LabelPowerset GNB')
-        predictions = classifier.predict(x_test)
-        print(f'Multilabel LabelPowerset GNB accuracy = \n{accuracy_score(predictions, y_test)}')
-        print(f'Multilabel LabelPowerset GNB classification report = \n{classification_report(predictions, y_test)}')
-
-        print('Training model: LabelPowerset SVM')
-        # serial vector machine base classifier
-        classifier = LabelPowerset(SVC())
-        classifier.fit(x_train, y_train)
-        print('Predictions: LabelPowerset SVM')
-        predictions = classifier.predict(x_test)
-        print(f'Multilabel LabelPowerset SVM accuracy = \n{accuracy_score(predictions, y_test)}')
-        print(f'Multilabel LabelPowerset SVM classification report = \n{classification_report(predictions, y_test)}')
-
-        print('Training model: MLkNN')
-        classifier = MLkNN(k=21)
-        classifier.fit(x_train.values, y_train.values)
-
-        print('Predictions: MLkNN')
-        predictions = classifier.predict(x_test)
-        print(f'Multilabel MLkNN accuracy = \n{accuracy_score(predictions, y_test)}')
-        print(f'Multilabel MLkNN classification report = \n{classification_report(predictions, y_test)}')
-        # print(f'Multilabel MLkNN confusion matrix = \n{confusion_matrix(predictions, y_test)}')
-
-        print('--End of pH multilabel classification--')
+        print(f'--End of single feature binary multi-class classification for: {features}--')
 
     def _svn_classify(self, features: List, multilabel=False, plot=False):
-        print(f'--Start of SVN classification for: {features} binary: {multilabel}--')
+        print(f'--Start of SVN classification for: {features} multilabel: {multilabel}--')
         x = self.data.drop([GroundTruthBuilder.PH_GENERIC_CLASS, GroundTruthBuilder.PH_VEG_ROW_CROPS_CLASS,
                             GroundTruthBuilder.PH_FRUITS_NUTS_CLASS, GroundTruthBuilder.EC_GENERIC_CLASS], axis=1)
-        x = x[[features[0]]]
+        x = x[features]
         y = self.data[GroundTruthBuilder.FEATURES_TO_LABELS_DICT[features[0]][0]]
         x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.30, random_state=31)
 
@@ -119,10 +66,10 @@ class SoilClassifier(object):
         print(f'SVC confusion matrix = \n{confusion_matrix(svc_preds, y_test)}')
         print(f'SVC classification report = \n{classification_report(svc_preds, y_test)}')
 
-        print(f'--End of SVN classification for: {features} binary: {multilabel}--')
+        print(f'--End of SVN classification for: {features} multilabel: {multilabel}--')
 
     def _knn_classify(self, features: List, multilabel=False, plot=False):
-        print(f'--Start of k-NN classification for: {features} binary: {multilabel}--')
+        print(f'--Start of k-NN classification for: {features} multilabel: {multilabel}--')
         x = self.data.drop([GroundTruthBuilder.PH_GENERIC_CLASS, GroundTruthBuilder.PH_VEG_ROW_CROPS_CLASS,
                             GroundTruthBuilder.PH_FRUITS_NUTS_CLASS, GroundTruthBuilder.EC_GENERIC_CLASS], axis=1)
         x = x[features]
@@ -165,4 +112,116 @@ class SoilClassifier(object):
             )
             plt.show()
 
-        print(f'--end of k-NN classification for: {features} binary: {multilabel}--')
+        print(f'--end of k-NN classification for: {features} multilabel: {multilabel}--')
+
+
+class MultilabelClassifier(object):
+    def __init__(self, data):
+        self.data = data
+
+    def single_feature_multilabel_classify(self, features: List):
+        print(f'--Start of single feature multi-label classification for: {features}--')
+
+        for feature in features:
+            self._binary_relevance_classify(features=[feature], plot=False)
+            self._classifier_chain_classify(features=[feature], plot=False)
+            self._label_power_set_classify(features=[feature], plot=False)
+            self._multi_learn_knn_classify(features=[feature], plot=False)
+
+        print(f'--End of single feature multi-label classification for: {features}--')
+
+    def _binary_relevance_classify(self, features: List, plot=False):
+        print(f'--Start of Binary Relevance multilabel classification for: {features}--')
+        x = self.data.drop([GroundTruthBuilder.PH_GENERIC_CLASS, GroundTruthBuilder.PH_VEG_ROW_CROPS_CLASS,
+                            GroundTruthBuilder.PH_FRUITS_NUTS_CLASS], axis=1)
+        x = x[features]
+        labels = GroundTruthBuilder.FEATURES_TO_LABELS_DICT[features[0]][1:]
+        y = self.data[labels]
+
+        x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.30, random_state=31)
+
+        print('Training model: BinaryRelevance')
+        # initialize binary relevance multi-label classifier with a gaussian naive bayes base classifier
+        classifier = BinaryRelevance(GaussianNB())
+        classifier.fit(x_train, y_train)
+        print('Predictions: BinaryRelevance')
+        predictions = classifier.predict(x_test)
+        print(f'Multilabel BinaryRelevance accuracy = \n{accuracy_score(predictions, y_test)}')
+        print(f'Multilabel BinaryRelevance classification report = \n{classification_report(predictions, y_test)}')
+        # print(f'Multilabel confusion matrix = \n{confusion_matrix(predictions, y_test)}')
+
+        print(f'--End of Binary Relevance multilabel classification for: {features}--')
+
+    def _classifier_chain_classify(self, features: List, plot=False):
+        print(f'--Start of Classifier Chain multilabel classification for: {features}--')
+
+        x = self.data.drop([GroundTruthBuilder.PH_GENERIC_CLASS, GroundTruthBuilder.PH_VEG_ROW_CROPS_CLASS,
+                            GroundTruthBuilder.PH_FRUITS_NUTS_CLASS], axis=1)
+        x = x[features]
+        labels = GroundTruthBuilder.FEATURES_TO_LABELS_DICT[features[0]][1:]
+        y = self.data[labels]
+
+        x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.30, random_state=31)
+
+        print('Training model: ClassifierChain')
+        classifier = ClassifierChain(GaussianNB())
+        diff = set(y_test) - set(y_train)
+        classifier.fit(x_train, y_train)
+        print('Predictions: ClassifierChain')
+        predictions = classifier.predict(x_test)
+        print(f'Multilabel ClassifierChain accuracy = \n{accuracy_score(predictions, y_test)}')
+        print(f'Multilabel ClassifierChain classification report = \n{classification_report(predictions, y_test)}')
+
+        print(f'--End of Classifier Chain multilabel classification for: {features}--')
+
+    def _label_power_set_classify(self, features: List, plot=False):
+        print(f'--Start of Label Power Set multilabel classification for: {features}--')
+
+        x = self.data.drop([GroundTruthBuilder.PH_GENERIC_CLASS, GroundTruthBuilder.PH_VEG_ROW_CROPS_CLASS,
+                            GroundTruthBuilder.PH_FRUITS_NUTS_CLASS], axis=1)
+        x = x[features]
+        labels = GroundTruthBuilder.FEATURES_TO_LABELS_DICT[features[0]][1:]
+        y = self.data[labels]
+
+        x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.30, random_state=31)
+
+        classifier = LabelPowerset(GaussianNB())
+        classifier.fit(x_train, y_train)
+        print('Predictions: LabelPowerset GNB')
+        predictions = classifier.predict(x_test)
+        print(f'Multilabel LabelPowerset GNB accuracy = \n{accuracy_score(predictions, y_test)}')
+        print(f'Multilabel LabelPowerset GNB classification report = \n{classification_report(predictions, y_test)}')
+
+        print('Training model: LabelPowerset SVM')
+        # serial vector machine base classifier
+        classifier = LabelPowerset(SVC())
+        classifier.fit(x_train, y_train)
+        print('Predictions: LabelPowerset SVM')
+        predictions = classifier.predict(x_test)
+        print(f'Multilabel LabelPowerset SVM accuracy = \n{accuracy_score(predictions, y_test)}')
+        print(f'Multilabel LabelPowerset SVM classification report = \n{classification_report(predictions, y_test)}')
+
+        print(f'--End of Label Power Set multilabel classification for: {features}--')
+
+    def _multi_learn_knn_classify(self, features: List, plot=False):
+        print(f'--Start of Multi-learn kNN multilabel classification for: {features}--')
+
+        x = self.data.drop([GroundTruthBuilder.PH_GENERIC_CLASS, GroundTruthBuilder.PH_VEG_ROW_CROPS_CLASS,
+                            GroundTruthBuilder.PH_FRUITS_NUTS_CLASS], axis=1)
+        x = x[features]
+        labels = GroundTruthBuilder.FEATURES_TO_LABELS_DICT[features[0]][1:]
+        y = self.data[labels]
+
+        x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.30, random_state=31)
+
+        print('Training model: MLkNN')
+        classifier = MLkNN(k=21)
+        classifier.fit(x_train.values, y_train.values)
+
+        print('Predictions: MLkNN')
+        predictions = classifier.predict(x_test)
+        print(f'Multilabel MLkNN accuracy = \n{accuracy_score(predictions, y_test)}')
+        print(f'Multilabel MLkNN classification report = \n{classification_report(predictions, y_test)}')
+        # print(f'Multilabel MLkNN confusion matrix = \n{confusion_matrix(predictions, y_test)}')
+
+        print(f'--End of Label Multi-learn kNN multilabel classification for: {features}--')
